ee8a7f24113c059f0e6e41bbf39569dcc34d3c6c
SSAISANKEERTH <48541342+SSAISANKEERTH@users.noreply.github.com>
2019-05-23 15:21:13 +1000

    Button Click directs player to Main Game

---
+++ MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource MusicSource;
+    public AudioClip MusicClip;
+     void Start()
+    {
+        MusicSource.clip = MusicClip;   
+    }
+     void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MusicSource.Play();
+            
+
+        }
+    }
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Game");
+
+    }
+}
